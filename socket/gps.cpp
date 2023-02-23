85c61bccb38c9386247a8ad146d6f919cc0a4db1
take1014 <y.takehara1014@gmail.com>
2023-02-23 20:33:36 +0900

    add nmea

--- socket/gps.cpp
+++ socket/gps.cpp
@@ -5,8 +5,13 @@
 
 #include "gps.hpp"
 
-constexpr int DATA_SZ_GPGGA = 16;
+constexpr int DATA_SZ_GPZDA =  8;
+constexpr int DATA_SZ_GPGLL =  9;
+constexpr int DATA_SZ_GPVTG = 11;
 constexpr int DATA_SZ_GPRMC = 14;
+constexpr int DATA_SZ_GPGGA = 16;
+constexpr int DATA_SZ_GPGSA = 19;
+constexpr int DATA_SZ_GPGSV = 21;
 
 static std::vector<std::string>
 split(const std::string &data)
@@ -58,7 +63,6 @@ static bool
 parseGPRMC(const std::vector<std::string> &splitted_data, Json::Value &gprmc)
 {
     if(splitted_data.size() != DATA_SZ_GPRMC){ return false; }
-
     gprmc["time_utc"]                     = calcUTC(splitted_data.at(1));
     gprmc["status"]                       = splitted_data.at(2);
     gprmc["latitude"]                     = calcDecimalDegrees(splitted_data.at(3));
@@ -74,6 +78,105 @@ parseGPRMC(const std::vector<std::string> &splitted_data, Json::Value &gprmc)
     gprmc["checksum"]                     = convToIntFromHex(splitted_data.at(13));
     return true;
 }
+
+static bool
+parseGPGLL(const std::vector<std::string> &splitted_data, Json::Value &gpgll)
+{
+    if(splitted_data.size() != DATA_SZ_GPGLL){ return false; }
+    gpgll["latitude"]       = calcDecimalDegrees(splitted_data.at(1));
+    gpgll["lat_direction"]  = splitted_data.at(2);
+    gpgll["longitude"]      = calcDecimalDegrees(splitted_data.at(3));
+    gpgll["long_direction"] = splitted_data.at(4);
+    gpgll["time_utc"]       = calcUTC(splitted_data.at(5));
+    gpgll["status"]         = splitted_data.at(6);
+    gpgll["mode"]           = splitted_data.at(7);
+    gpgll["checksum"]       = convToIntFromHex(splitted_data.at(8));
+    return true;
+}
+
+static bool
+parseGPGSA(const std::vector<std::string> &splitted_data, Json::Value &gpgsa)
+{
+    if(splitted_data.size() != DATA_SZ_GPGSA){ return false; }
+    gpgsa["mode"]            = splitted_data.at(1);
+    gpgsa["status"]          = convToIntFromDecimal(splitted_data.at(2));
+    gpgsa["satellite_id_1"]  = convToIntFromDecimal(splitted_data.at(3));
+    gpgsa["satellite_id_2"]  = convToIntFromDecimal(splitted_data.at(4));
+    gpgsa["satellite_id_3"]  = convToIntFromDecimal(splitted_data.at(5));
+    gpgsa["satellite_id_4"]  = convToIntFromDecimal(splitted_data.at(6));
+    gpgsa["satellite_id_5"]  = convToIntFromDecimal(splitted_data.at(7));
+    gpgsa["satellite_id_6"]  = convToIntFromDecimal(splitted_data.at(8));
+    gpgsa["satellite_id_7"]  = convToIntFromDecimal(splitted_data.at(9));
+    gpgsa["satellite_id_8"]  = convToIntFromDecimal(splitted_data.at(10));
+    gpgsa["satellite_id_9"]  = convToIntFromDecimal(splitted_data.at(11));
+    gpgsa["satellite_id_10"] = convToIntFromDecimal(splitted_data.at(12));
+    gpgsa["satellite_id_11"] = convToIntFromDecimal(splitted_data.at(13));
+    gpgsa["satellite_id_12"] = convToIntFromDecimal(splitted_data.at(14));
+    gpgsa["pdop"]            = convToFloat(splitted_data.at(15));
+    gpgsa["hdop"]            = convToFloat(splitted_data.at(16));
+    gpgsa["vdop"]            = convToFloat(splitted_data.at(17));
+    gpgsa["checksum"]        = convToIntFromHex(splitted_data.at(18));
+    return true;
+}
+
+static bool
+parseGPGSV(const std::vector<std::string> &splitted_data, Json::Value &gpgsv)
+{
+    if(splitted_data.size() != DATA_SZ_GPGSV){ return false; }
+    gpgsv["total_message_num"]      = convToIntFromDecimal(splitted_data.at(1));
+    gpgsv["message_number"]         = convToIntFromDecimal(splitted_data.at(2));
+    gpgsv["total_satellite_number"] = convToFloat(splitted_data.at(3));
+    gpgsv["satellite_number_1"]     = convToIntFromDecimal(splitted_data.at(4));
+    gpgsv["elevation_1"]            = convToFloat(splitted_data.at(5));
+    gpgsv["azimuth_1"]              = convToFloat(splitted_data.at(6));
+    gpgsv["sn_1"]                   = convToFloat(splitted_data.at(7));
+    gpgsv["satellite_number_2"]     = convToIntFromDecimal(splitted_data.at(8));
+    gpgsv["elevation_2"]            = convToFloat(splitted_data.at(9));
+    gpgsv["azimuth_2"]              = convToFloat(splitted_data.at(10));
+    gpgsv["sn_2"]                   = convToFloat(splitted_data.at(11));
+    gpgsv["satellite_number_3"]     = convToIntFromDecimal(splitted_data.at(12));
+    gpgsv["elevation_3"]            = convToFloat(splitted_data.at(13));
+    gpgsv["azimuth_3"]              = convToFloat(splitted_data.at(14));
+    gpgsv["sn_3"]                   = convToFloat(splitted_data.at(15));
+    gpgsv["satellite_number_4"]     = convToIntFromDecimal(splitted_data.at(16));
+    gpgsv["elevation_4"]            = convToFloat(splitted_data.at(17));
+    gpgsv["azimuth_4"]              = convToFloat(splitted_data.at(18));
+    gpgsv["sn_4"]                   = convToFloat(splitted_data.at(19));
+    gpgsv["checksum"]               = convToIntFromHex(splitted_data.at(20));
+    return true;
+}
+
+static bool
+parseGPVTG(const std::vector<std::string> &splitted_data, Json::Value &gpvtg)
+{
+    if(splitted_data.size() != DATA_SZ_GPVTG){ return false; }
+    gpvtg["track"]               = convToFloat(splitted_data.at(1));
+    gpvtg["track_mode"]          = splitted_data.at(2);
+    gpvtg["magnetic_track"]      = convToFloat(splitted_data.at(3));
+    gpvtg["magnetic_track_mode"] = splitted_data.at(4);
+    gpvtg["ground_speed_knots"]  = convToFloat(splitted_data.at(5));
+    gpvtg["khots"]               = splitted_data.at(6);
+    gpvtg["ground_speed_kmh"]    = convToFloat(splitted_data.at(7));
+    gpvtg["kmh"]                 = splitted_data.at(8);
+    gpvtg["mode"]                = splitted_data.at(9);
+    gpvtg["checksum"]            = convToIntFromHex(splitted_data.at(10));
+    return true;
+}
+
+static bool
+parseGPZDA(const std::vector<std::string> &splitted_data, Json::Value &gpzda)
+{
+    if(splitted_data.size() != DATA_SZ_GPZDA){ return false; }
+    gpzda["time_utc"]                       = calcUTC(splitted_data.at(1));
+    gpzda["day"]                            = convToIntFromDecimal(splitted_data.at(2));
+    gpzda["month"]                          = convToIntFromDecimal(splitted_data.at(3));
+    gpzda["year"]                           = convToIntFromDecimal(splitted_data.at(4));
+    gpzda["local_zone_hours_description"]   = convToIntFromDecimal(splitted_data.at(5));
+    gpzda["local_zone_minutes_description"] = convToIntFromDecimal(splitted_data.at(6));
+    gpzda["checksum"]                       = convToIntFromHex(splitted_data.at(7));
+    return true;
+}
+
 
 GPS::GPS() : m_device(""),
              m_baudrate(Serial::eB9600),
@@ -123,7 +226,7 @@ GPS::event_loop(void)
                 continue;
             }
             /* TODO: parser */
-            /* GPGGA GPGLL GPGSA GPGSV GPRMC GPVTG PGZDA */
+            /* GPZDA */
             std::vector<std::string> splitted_data = split(line.substr(1, line.size()-2));
 
             if (splitted_data.front() == "GPGGA")
@@ -143,10 +246,38 @@ GPS::event_loop(void)
                     std::cout << gprmc_json << std::endl;
                 }
             }
+
+            if (splitted_data.front() == "GPGLL")
+            {
+                if (parseGPGLL(splitted_data, gnss_data["GPGLL"]))
+                {
+                    std::string gpgll_json =  "\"GPGLL\":{\"latitude\":" + gnss_data["GPGLL"]["latitude"].asString() + "," + "\"longitude\":" + gnss_data["GPGLL"]["longitude"].asString() + "}";
+                    std::cout << gpgll_json << std::endl;
+                }
+            }
+
+            if (splitted_data.front() == "GPGSA")
+            {
+                parseGPGSA(splitted_data, gnss_data["GPGSA"]);
+            }
+
+            if (splitted_data.front() == "GPGSV")
+            {
+                parseGPGSV(splitted_data, gnss_data["GPGSV"]);
+            }
+
+            if (splitted_data.front() == "GPVTG")
+            {
+                parseGPVTG(splitted_data, gnss_data["GPVTG"]);
+            }
+
+            if (splitted_data.front() == "GPZDA")
+            {
+                parseGPZDA(splitted_data, gnss_data["GPZDA"]);
+            }
         }
 
         /* websocket */
-
     }
 }
 
