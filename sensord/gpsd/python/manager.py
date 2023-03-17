3eab272c9686ec91724d409ff1a75c143f8f059b
take1014 <y.takehara1014@gmail.com>
2023-03-17 18:32:18 +0900

    updatea manager

--- sensord/gpsd/python/manager.py
+++ sensord/gpsd/python/manager.py
@@ -8,25 +8,27 @@ from gps import GPS
 from nmea_parser import NMEAParser
 
 def run_gps()->None:
-    # gps = GPS(serial_port="/dev/ttyUSB0")
-    gps = GPS(serial_port="/dev/ttyACM0")
+    gps = GPS(serial_port="/dev/ttyUSB0")
+    #gps = GPS(serial_port="/dev/ttyACM0")
     parser = NMEAParser()
 
-    nmea_cnt = 0
     # TODO: must define extrinsic parameter
     # Change the value to 6 if you use a GPS that supports
     # both GPGSA and GNGSA reception
     sizeof_notlist_nmea = 5 # default: RMC, VTG, GGA, GLL, GSA (excluse GSV)
+
+    # NMEA counter and flags
+    # processing nmea counter
+    nmea_cnt = 0
     total_nmea_sz = sizeof_notlist_nmea    # total count include GPGSV
     # Initialize flags
     isinit_gpgsv = False
     isinit_glgsv = False
     isinit_gpgsa = False
     isinit_gngsa = False
-    issync = False
+    issync       = False
 
     nmea_json = dict()
-
     while True:
         # get message from serial.
         msg  = gps.recvMsg()
@@ -37,73 +39,92 @@ def run_gps()->None:
 
         if nmea_type == None or nmea_type[2:] == "TXT": continue
         if nmea_cnt == 0:
-            issync = True if nmea_type[2:] == "GSV" and parsed_nmea["message_number"] == 1 else False
+            # issync = True if nmea_type[2:] == "GSV" and parsed_nmea["message_number"] == 1 else False
+            issync = True if nmea_type[2:] == "GGA" else False
+            # and parsed_nmea["message_number"] == 1 else False
 
         #  Not process if out of sync
         if not issync: continue
 
-        print(msg.strip())
-        nmea_json = {"GPS_OUTPUT":{}, "RAW_GNSS":{}} if nmea_cnt == 0 else nmea_json
-        if nmea_type == "GPGSV" :
-            if not isinit_gpgsv:
-                # Initialize GPGSV
-                nmea_json["RAW_GNSS"][nmea_type] = []
-                total_nmea_sz += parsed_nmea["total_message_num"]
-                isinit_gpgsv = True
-            # needs processing per sattelite.
-            nmea_json["RAW_GNSS"][nmea_type].append(parsed_nmea)
-            nmea_cnt += 1
-
-        elif nmea_type == "GLGSV":
-            # TODO: create parser
-            if not isinit_glgsv:
-                # Initialize GPGSV
-                nmea_json["RAW_GNSS"][nmea_type] = []
-                total_nmea_sz += parsed_nmea["total_message_num"]
-                isinit_glgsv = True
-            # needs processing per sattelite.
-            nmea_json["RAW_GNSS"][nmea_type].append(parsed_nmea)
-            nmea_cnt += 1
-
-        elif nmea_type == "GPGSA":
-            # There are more than 12 satellites,
-            # but since they are not used, only one type of GSA is saved.
-            if not isinit_gpgsa:
-                nmea_json["RAW_GNSS"][nmea_type] = parsed_nmea
-                isinit_gpgsa = True
+        try:
+            nmea_json = {"GPS_OUTPUT":{}, "RAW_GNSS":{}} if nmea_cnt == 0 else nmea_json
+            if nmea_type == "GPGSV" :
+                if not isinit_gpgsv:
+                    # Initialize GPGSV
+                    nmea_json["RAW_GNSS"][nmea_type] = []
+                    total_nmea_sz += parsed_nmea["total_message_num"]
+                    isinit_gpgsv = True
+                # needs processing per sattelite.
+                nmea_json["RAW_GNSS"][nmea_type].append(parsed_nmea)
+                print(msg.strip())
                 nmea_cnt += 1
 
-        elif nmea_type == "GNGSA":
-            # There are more than 12 satellites,
-            # but since they are not used, only one type of GSA is saved.
-            if not isinit_gngsa:
-                nmea_json["RAW_GNSS"][nmea_type] = parsed_nmea
-                isinit_gngsa = True
+            elif nmea_type == "GLGSV":
+                # TODO: create parser
+                if not isinit_glgsv:
+                    # Initialize GPGSV
+                    nmea_json["RAW_GNSS"][nmea_type] = []
+                    total_nmea_sz += parsed_nmea["total_message_num"]
+                    isinit_glgsv = True
+                # needs processing per sattelite.
+                nmea_json["RAW_GNSS"][nmea_type].append(parsed_nmea)
+                print(msg.strip())
                 nmea_cnt += 1
 
-        # RMC, VTG, GGA, GLL
-        else:
-            nmea_json["RAW_GNSS"][nmea_type] = parsed_nmea
-            # increment saved nmea count
-            nmea_cnt += 1
+            elif nmea_type == "GPGSA":
+                # There are more than 12 satellites,
+                # but since they are not used, only one type of GSA is saved.
+                if not isinit_gpgsa:
+                    nmea_json["RAW_GNSS"][nmea_type] = parsed_nmea
+                    print(msg.strip())
+                    isinit_gpgsa = True
+                    nmea_cnt += 1
 
-        if (nmea_cnt == total_nmea_sz) and (total_nmea_sz != sizeof_notlist_nmea):
-            nmea_json["GPS_OUTPUT"]["key"] = gps.hostname
-            nmea_json["GPS_OUTPUT"]["latitude"]  = nmea_json["RAW_GNSS"]["GPRMC"]["latitude"]
-            nmea_json["GPS_OUTPUT"]["longitude"] = nmea_json["RAW_GNSS"]["GPRMC"]["longitude"]
-            # write Log
-            gps.writeLog(json.dumps(nmea_json))
-            # publish to mqtt
-            gps.publish(json.dumps(nmea_json))
-            # initialize counter
+            elif nmea_type == "GNGSA":
+                # There are more than 12 satellites,
+                # but since they are not used, only one type of GSA is saved.
+                if not isinit_gngsa:
+                    nmea_json["RAW_GNSS"][nmea_type] = parsed_nmea
+                    print(msg.strip())
+                    isinit_gngsa = True
+                    nmea_cnt += 1
+
+            # RMC, VTG, GGA, GLL
+            else:
+                nmea_json["RAW_GNSS"][nmea_type] = parsed_nmea
+                print(msg.strip())
+                # increment saved nmea count
+                nmea_cnt += 1
+
+            if (nmea_cnt == total_nmea_sz) and (total_nmea_sz != sizeof_notlist_nmea):
+                nmea_json["GPS_OUTPUT"]["key"] = gps.hostname
+                nmea_json["GPS_OUTPUT"]["latitude"]  = nmea_json["RAW_GNSS"]["GNGGA"]["latitude"]
+                nmea_json["GPS_OUTPUT"]["longitude"] = nmea_json["RAW_GNSS"]["GNGGA"]["longitude"]
+                # write Log
+                gps.writeLog(json.dumps(nmea_json))
+                # publish to mqtt
+                gps.publish(json.dumps(nmea_json))
+                # initialize counter and flags
+                nmea_cnt = 0
+                total_nmea_sz = sizeof_notlist_nmea
+                isinit_gpgsv = False
+                isinit_glgsv = False
+                isinit_gpgsa = False
+                isinit_gngsa = False
+                issync       = False
+
+        except Exception as e:
+            print("Occur EXCEPTION: {}".format(e.args))
+            gps.writeLog(e.args)
+            # initialize counter and flags
             nmea_cnt = 0
             total_nmea_sz = sizeof_notlist_nmea
-            # Initialize init_flags
-            isinit_gpgsv=False
-            isinit_glgsv=False
-            isinit_gpgsa=False
-            isinit_gngsa=False
-            issync = False
+            isinit_gpgsv = False
+            isinit_glgsv = False
+            isinit_gpgsa = False
+            isinit_gngsa = False
+            issync       = False
+            continue
 
 def main() -> None:
     # TODO:add proccesing thread
